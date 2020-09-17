1f4b68b3cf796fe4de48ae7cff30cfe6eb410c67
kevinksit <kevinkamansit@gmail.com>
2020-09-16 18:55:24 -0700

    Added passing of steps_per_rev to the driver and removed it from constants.

--- _Classes/Drivers/StepperMotorDriver.m
+++ _Classes/Drivers/StepperMotorDriver.m
@@ -1,21 +1,24 @@
 classdef StepperMotorDriver < NIDAQDriver
-	% Updated 15Sep2020 KS | Added STEPS_PER_REV from the StepperController, because it fits here better...
+	% Updated 15Sep2020 KS | Added steps_per_rev from the StepperController, because it fits here better...
 	
-	properties (Constant = true)
-		STEPS_PER_REV = 200;
-	end
-
 	properties
 		step_line
 		dir_line
 		microstep_lines
+
+		steps_per_rev
 	end
 		
 	methods
-		function obj = StepperMotorDriver(lines)
+		function obj = StepperMotorDriver(lines, steps_per_rev)
 			if nargin < 1 || isempty(lines)
 				lines = obj.inputPorts({'step', 'direction'});
 			end
+
+			if nargin < 2 || isempty(steps_per_rev)
+				steps_per_rev = 200;
+			end
+			obj.steps_per_rev = steps_per_rev;
 			obj.step_line = lines{1};
 			obj.dir_line = lines{2};
 			obj.microstep_lines = lines(3:end);
@@ -39,7 +42,7 @@ classdef StepperMotorDriver < NIDAQDriver
 		end
 
 		function out = getStepsPerRev(obj)
-			out = obj.STEPS_PER_REV;
+			out = obj.steps_per_rev;
 		end
 	end
 end
